f18cdae1d9a6d8f27bfeec0469304a3f4eb8dd73
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-05-04 09:15:05 +0530

    Create Day1-dsa.java

---
+++ Day1-dsa.java
@@ -0,0 +1,20 @@
+
+class Main {
+    public static void main(String[] args) {
+        
+        // System.out.print(2+'A');
+        // System.out.println();
+        // System.out.println(2+2.2);
+        
+        
+        System.out.println(1+1);
+        System.out.println('a'+1);
+        System.out.println('a'+1+"a"+123);
+        System.out.println('a'+1+"a"+true);
+        
+        //error : cant add boolean
+        // System.out.println(true+false);
+        // System.out.println(true + "false");
+        
+    }
+}
