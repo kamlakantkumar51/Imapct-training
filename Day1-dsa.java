cc1188dfc123982aeb6b481eed08ee5c94ea1096
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-05-04 09:27:16 +0530

    Update Day1-dsa.java

--- Day1-dsa.java
+++ Day1-dsa.java
@@ -6,11 +6,29 @@ class Main {
         // System.out.println();
         // System.out.println(2+2.2);
         
+
+
+
+
+
+        int a = 10;
+        char b  = 'a';
+        boolean c = true;
+        float f = 123.231f;
+        double d = 123.123;
+        String s = "asus tuf";
         
-        System.out.println(1+1);
-        System.out.println('a'+1);
-        System.out.println('a'+1+"a"+123);
-        System.out.println('a'+1+"a"+true);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(s);
+        
+        // System.out.println(1+1);
+        // System.out.println('a'+1);
+        // System.out.println('a'+1+"a"+123);
+        // System.out.println('a'+1+"a"+true);
         
         //error : cant add boolean
         // System.out.println(true+false);
