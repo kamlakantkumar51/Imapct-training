917d297b1656bc0478e76e93fa6be9f6b9b427ca
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-05-06 15:57:57 +0530

    Update Day-3-dsa.java

--- Day-3-dsa.java
+++ Day-3-dsa.java
@@ -27,3 +27,17 @@ class Main {
         }
     }
 }
+// class Main {
+//     public static void main(String[] args) {
+//         //2d array print
+//         //wap to sum of all elemnts in an matrix
+//         int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        
+//         for(int i=0;i<3;i++){
+//             for(int j=0;j<4;j++){
+//                 System.out.print(arr[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
