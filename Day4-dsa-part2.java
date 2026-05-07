d3f58f957c554217deedd1ff421e2e63bda617ef
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-05-07 13:10:22 +0530

    Create Day4-dsa-part2.java

---
+++ Day4-dsa-part2.java
@@ -0,0 +1,26 @@
+class Main {
+    public static void main(String[] args) {
+        //for loop
+        // for(int i=1;i<=5;i++){
+        //     System.out.println(i + " ");
+        // }
+        
+        //while loop
+        int i=1;
+        while(i<=10){
+            if(i%2 ==0){
+              System.out.println(i + " ");   
+            }
+            i++;
+        }
+        
+        //do while loop
+        // int i=1;
+        // do{
+        //     System.out.println(i + " ");
+        //     i++;
+        // }while(i<=5);
+        
+        
+    }
+}
