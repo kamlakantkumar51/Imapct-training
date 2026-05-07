ce3182e06ad6c1518256a981b7fdd65ab6aa6e9c
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2026-05-07 14:01:29 +0530

    Update Day4-dsa-part2.java

--- Day4-dsa-part2.java
+++ Day4-dsa-part2.java
@@ -24,3 +24,25 @@ class Main {
         
     }
 }
+
+
+
+public class Main {
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1); 
+    }
+    public static void main(String[] args) {
+        //int n = 5;
+        // System.out.println(factorial(n));
+        int num = 5789;
+        while(num > 0){
+            int digit = num%10;
+            System.out.println(digit);
+            //int sum = sum+digit;
+            num = num/10;
+        }
+    }
+}
